7a520f1442de7d5b6fe492dcf63a9e17d3525f1c
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-06-06 11:20:49 +0900

    Meet requirements for assignment 2

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,10 +5,12 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
 
-DATABASE_URI = "sqlite:///web_app_99.db"  # using relative filepath
+# DATABASE_URI = "sqlite:///web_app_99.db"  # using relative filepath
 # DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
 # DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+DATABASE_URI = 'sqlite:///twitoff_development_pt5.db'  # using relative filepath
 
 
 def create_app():
@@ -20,6 +22,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
 
     return app
 
